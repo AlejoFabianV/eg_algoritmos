4442b8200756f78a0d755880d42d229f8f861fd5
alejo fabian <verhagenalejo@gmail.com>
2025-09-09 17:01:15 -0300

    ej parcial 2

--- parcial2.py
+++ parcial2.py
@@ -139,34 +139,205 @@ crear un prg.ppaL que use esta funcion
 
 '''
 
-def cadena(pfrase):
-    # pfrase = apellidp#ciudad#edad#especialidad
-    c = int()
-    e = int()
-    vi = int()
-    largo_frase = int()
-    vcadena = [str()] * 4
-    auxicad = str()
+# def cadena(pfrase):
+#     # pfrase = apellidp#ciudad#edad#especialidad
+#     c = int()
+#     e = int()
+#     vi = int()
+#     largo_frase = int()
+#     vcadena = [str()] * 4
+#     auxicad = str()
 
-    largo_frase = len(pfrase)
-    for c in range(0, largo_frase):
-        if pfrase[c] == "#":
-            auxicad = pfrase[vi:c]
-            vcadena[e] = auxicad
-            vi = c + 1
-            e = e + 1
-    auxicad = pfrase[vi:largo_frase]
-    vcadena[e] = auxicad
-    return(vcadena)
+#     largo_frase = len(pfrase)
+#     for c in range(0, largo_frase):
+#         if pfrase[c] == "#":
+#             auxicad = pfrase[vi:c]
+#             vcadena[e] = auxicad
+#             vi = c + 1
+#             e = e + 1
+#     auxicad = pfrase[vi:largo_frase]
+#     vcadena[e] = auxicad
+#     return(vcadena)
 
-#prg.ppal
-frase = str()
-x = int()
-vfrase = [str()] * 4
+# #prg.ppal
+# frase = str()
+# x = int()
+# vfrase = [str()] * 4
 
-frase = input("ingrese frase: ")
-vfrase = cadena(frase)
-for x in range(0,4):
-    print(vfrase[x])
+# frase = input("ingrese frase: ")
+# vfrase = cadena(frase)
+# for x in range(0,4):
+#     print(vfrase[x])
 
 
+'''
+Crear funcion que recibre como parametro un vector de 50 elementos de Str() cargado con frases, Mostar: mostrar frases que tienen mayor cantidad de consonantes 
+'''
+
+'''
+Crear una funcion que recibe una cadena como parametro de entrada, debera retornar la cantidad de veces que se repite cada vocal
+sin discriminar entre mayusculas y minusculas
+'''
+# def vocales(pfrase):
+#     x = int()
+#     ca = ce = ci = co = cu = int()
+#     largo_frase = int()
+
+#     largo_frase = len(pfrase)
+#     for x in range(0,largo_frase):
+#         match pfrase[x]:
+#             case "A":
+#                 ca = ca + 1
+#             case "E":
+#                 ce = ce + 1
+#             case "I":
+#                 ci = ci + 1
+#             case "O":
+#                 co = co + 1
+#             case "U":
+#                 cu = cu + 1
+#     return(ca,ce, ci, co, cu )
+
+# frase = input("ingresa frase: ")
+# frase = vocales(frase)
+# print(frase)
+
+'''
+Cargar un vector int() de 100 elementos con numeros aleatorios comprenidos entre 1 y 1000, Mostar el vector ordenado
+'''
+
+# import random as r 
+# elementos = 10
+# vordenado = [int()] * elementos
+
+# for x in range(0, elementos):
+#     vordenado[x] = r.randint(1,1000)
+# vordenado.sort()
+# print(vordenado)
+
+'''
+Se ingresa con opcion a continuar los datos de las ventas realizadas por distinos venedores :
+    NroVendedor int
+    NroFactura  int
+    FechaVenta  str[10]
+    ImporteFactu real
+El codigo del vendedor esta comprendido entre 1-30
+Mostrar:
+    Cantidad de ventas realizadas por cada nro de vendedor
+    Total de importe por cada NroVendedor
+    Promedio de importe facturado por cada NroVendedor
+    NroVendedor con la mayor cantidad de ventas realizadas(puede ser un max repetido)
+    NroVendedor con el mayor total de importe facturado(puede ser un max repetido)
+
+'''
+# nroVendedor = int()
+# nroFactura = int()
+# fechaVenta = [str()] * 10
+# importeFactu = float()
+# otro = str()
+
+# max_v = [int()] * 31
+# max_i = [int()] * 31
+# x = int()
+# maxiVentas = int()
+# maxiImporte = int()
+# vnroVendedorMayor = [int()]
+# vnroVendedorMayorI = [int()]
+
+# while otro != "n":
+#     nroVendedor = int(input("ingrese nro de vendedor: "))
+#     if nroVendedor >= 1 and nroVendedor <= 30:
+#         nroFactura = int(input("Ingrese el nro de factura"))
+#         fecha_venta = input("ingrese fecha [dd/mm/aaaa]: ")
+#         max_v[nroVendedor] += 1
+#         importeFactu = float(input("ingrese el importe de facturacion: "))
+#         max_i[nroVendedor] += importeFactu
+#     else:
+#         print("nro de vendedor invalido")
+    
+#     otro = input("Quiere ingresar otro? ")
+
+# for x in range(1,31):
+#     if max_v[x] != 0:
+#         promedio = max_i[x]/max_v[x]
+#         print(f'Vendedor {x}, tiene {max_v[x]} venta/s y su importe es {max_i[x]}, su promedio es {promedio}')
+#         if max_v[x] > maxiVentas:
+#             maxiVentas = max_v[x]
+#             vnroVendedorMayor = [x]
+#         elif max_v[x] == maxiVentas:
+#             vnroVendedorMayor.append(x)
+#         if max_i[x] > maxiImporte:
+#             maxiImporte = max_i[x]
+#             vnroVendedorMayorI = [x]
+#         elif max_i[x] == maxiImporte:
+#             vnroVendedorMayorI.append(x)
+
+
+# print(f'El vendedor/res con mayor ventas fue/ron {vnroVendedorMayor}, con {rep_v} ventas')
+# print(f'El vendedor con mayor importe fue {vnroVendedorMayorI}')
+
+'''
+input con separacion
+'''
+# def cadena(pfrase):
+#     largo = int()
+#     x = int()
+#     fecha_m = valor_m = int()
+#     supervisor = turno = str()
+#     vi = int()
+#     e = int()
+#     auxic = int()
+#     vcad = [int()] * 4
+    
+#     largo = len(pfrase)
+#     for x in range(0, largo):
+#         if pfrase[x] == ";":
+#             auxic = pfrase[vi:x]
+#             vcad[e] = auxic
+#             vi = x + 1 
+#             e = e + 1
+#     auxic = pfrase[vi:largo]
+#     vcad[e] = auxic
+#     fecha_m = vcad[0]
+#     valor_m = vcad[1]
+#     supervisor = vcad[2]
+#     turno = vcad[3]
+
+#     return(fecha_m, valor_m, supervisor, turno)
+
+# frase = input("dd/mm/aaaa;valor;supervisor;turno : ")
+# datos = cadena(frase)
+# print(f'fecha: {datos[0]}, valor medio: {datos[1]}, supervisor: {datos[2]}, turno: {datos[3]}')
+
+'''
+Crear funcion que devueva la frase con mayor consonantes
+'''
+def consonantes(pfrase):
+    largo = int()
+    frase = str()
+    car = int()
+    cc = int()
+    auxi = [int()] * 2
+    x = y = w = z = int()
+    maxi = int()
+
+    for x in range(0,2):
+        frase = (pfrase).upper()
+        largo = len(frase[x])
+        for y in range(0,largo):
+            car = ord(frase[y])
+            if car >= 65 and car <= 90:
+                match frase[y]:
+                    case "A" | "E" | "I" | "O" | "U":
+                        print(" ")
+                    case _else :
+                        cc = cc + 1
+        auxi[x] = cc
+        cc = 0
+    for w in range(0,2):
+        if auxi[w] > maxi:
+            maxi = auxi[w]
+    for z in range(0,2):
+        if auxi[z] == maxi:
+            print(pfrase[z])
+    return
